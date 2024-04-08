9138f70752d106476cb27f216f42503eb53559fa
isaacgoncod <isaacgoncod@gmail.com>
2024-04-07 23:26:19 -0300

    Project setup

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Post::factory(100)->create();
+        Category::factory(5)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
