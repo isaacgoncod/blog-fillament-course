a8f94e0fd75c75e4c7a14167d0251903abd65152
isaacgoncod <isaacgoncod@gmail.com>
2024-05-16 15:38:05 -0300

    feat: SearchBox element in blog pagination

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -3,17 +3,41 @@
 namespace App\Livewire;
 
 use App\Models\Post;
-use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
+use Livewire\Attributes\Computed;
 
 class PostList extends Component
 {
     use WithPagination;
 
-    #[Computed()]
-    public function posts(){
-        return Post::published()->orderBy('published_at', 'desc')->paginate(3);
+    #[Url]
+    public $sort = 'desc';
+
+    public $search = '';
+
+    public function setSort($sort)
+    {
+        $this->sort = $sort === 'desc' ? 'desc' : 'asc';
+
+        $this->resetPage();
+    }
+
+   #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+    }
+
+    #[Computed]
+    public function posts()
+    {
+        return Post::published()
+            ->orderBy('published_at', $this->sort)
+            ->where('title', 'like', '%' . $this->search . '%')
+            ->simplePaginate(3);
     }
 
     public function render()
