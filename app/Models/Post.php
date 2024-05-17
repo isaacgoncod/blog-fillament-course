aff025bf4dd9926013c60b30dd033fe7ae48516b
isaacgoncod <isaacgoncod@gmail.com>
2024-05-17 09:57:58 -0300

    feat: Table categories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $casts = [
         'published_at' => 'datetime',
